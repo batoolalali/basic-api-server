4489deb60a6dc677532e310a5e2dabd4e4356007
batoolalali <abdullahbatool93@gmail.com>
2022-02-28 08:40:43 +0200

    added delete method

--- src/routes/clothes.js
+++ src/routes/clothes.js
@@ -8,7 +8,7 @@ router.get('/clothes', getAll);
 router.get('/clothes/:id', getOne);
 router.post('/clothes', addOne);
 router.put('/clothes/:id', updateOne);
-// router.delete('/clothes/:id', deleteOne);
+router.delete('/clothes/:id', deleteOne);
 
 async function getAll(req, res) {
     let clothes = await Clothes.findAll();
@@ -22,14 +22,16 @@ async function getOne(req, res) {
 
 async function updateOne(req, res) {
 
-  let  clothes = Clothes.update(req.body, { where: { id: parseInt(req.params.id) } })
-
-
-
+    let clothes = Clothes.update(req.body, { where: { id: parseInt(req.params.id) } })
     res.json(clothes);
 }
 async function addOne(req, res) {
     let clothes = await Clothes.create(req.body);
+    res.json(clothes);
+}
+
+async function deleteOne(req, res){
+    let clothes = await Clothes.destroy({where:{id : parseInt(req.params.id)}});
     res.json(clothes);
 }
 

ac93cf66a8f9c120b5839221a6a1735a48460c94
batoolalali <abdullahbatool93@gmail.com>
2022-03-02 09:58:45 +0200

    ssl

--- src/models/index.js
+++ src/models/index.js
@@ -7,11 +7,18 @@ const clothes = require('./clothes');
 const DATABASE_URL = process.env.DATABASE_URL;
 
 
-let sequelizeOptions = {
-   
-}
+let sequelizeOptions =  {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      }
+    }
+  }
 
-let sequelize = new Sequelize(DATABASE_URL);
+let sequelize = new Sequelize(POSTGRES_URL,sequelizeOptions);
+
+// let sequelize = new Sequelize(DATABASE_URL);
 
 
 module.exports={
